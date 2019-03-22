d6f746eb0de12183b6d05223b0a3c7fdfea9f565
xuebuchan <1020884375@qq.com>
2019-03-22 16:54:34 +0800

    修改

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -24,7 +24,7 @@
       <input type="text" class="form-control" v-model.lazy="motto" placeholder="v-model.lazy" />
     </form>
 
-    <a href="/pages/counter/main" class="counter">去往Vux示例页面</a>
+    <a href="/pages/counter/main" class="counter">去往Vux示例页面11111</a>
 
     <div class="all">
         <div class="left">
